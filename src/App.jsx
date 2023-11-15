fba78cbcc9c904736ff207e5764637016ef8f701
Melissa <lynch.melissat@gmail.com>
2023-11-15 12:55:01 -0500

    starter files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+
+function App() {
+
+  return (
+    <div className='main'>
+      <h1>Hello, World!</h1>
+      <img src="/src/assets/spongebob.gif"/>
+    </div>
+  )
+}
+
+export default App
